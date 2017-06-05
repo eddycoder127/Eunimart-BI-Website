1eb56b1b3d19c1dd9910ac708203b62f32771a7f
eddycoder127 <shreyashshriyam@gmail.com>
2017-06-05 21:16:49 +0530

    Eunimart Website

---
+++ src/main/java/com/eunimart/webapp/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.eunimart.webapp.service.impl;
+
+import java.sql.SQLException;
+
+import com.eunimart.webapp.dao.UserDao;
+import com.eunimart.webapp.service.UserService;
+
+public class UserServiceImpl implements UserService
+{
+
+		private UserDao userDao;
+
+		public UserDao getUserDao()
+		{
+				return this.userDao;
+		}
+
+		public void setUserDao(UserDao userDao)
+		{
+				this.userDao = userDao;
+		}
+
+		@Override
+		public boolean isValidUser(String username, String password) throws SQLException
+		{
+				return userDao.isValidUser(username, password);
+		}
+
+}
